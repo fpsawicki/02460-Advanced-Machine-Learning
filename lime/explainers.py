164f950bde4ff562eeda9640dbc5a72db7b15ac2
jgciudad <jgciudad@outlook.com>
2021-04-08 16:59:16 +0200

    fix visualize

--- lime/explainers.py
+++ lime/explainers.py
@@ -23,11 +23,15 @@ class ImageExplainer(Explainer):
         def get_seg_x(seg, x):
             return (seg == x) * 1
         
-        top_features = self.results[label]['feature_importance'][-n_top_features:] 
+        top_features = self.results[label]['feature_importance'][-n_top_features:]
+
+        top_indexes = []
+        for feature in top_features:
+            top_indexes.append(feature[0])
       
         explained_image = 0
-        for i in range(len(top_features)):
-          explained_image += get_seg_x(self.segs, top_features[i])
+        for i in range(len(top_indexes)):
+            explained_image += get_seg_x(self.segs, top_indexes[i])
       
         img_to_show = self.image.copy()
         img_to_show[explained_image == 0] = 0
